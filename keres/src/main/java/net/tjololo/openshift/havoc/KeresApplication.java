4f0c1fa829e6eabacf598b96852b44d26c645f26
Gaukstad, Vemund <vemund.gaukstad@brreg.no>
2016-12-19 14:32:55 +0100

    added keres module

---
+++ keres/src/main/java/net/tjololo/openshift/havoc/KeresApplication.java
@@ -0,0 +1,12 @@
+package net.tjololo.openshift.havoc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KeresApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KeresApplication.class, args);
+	}
+}
